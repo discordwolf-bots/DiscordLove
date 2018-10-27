1ed1922b419f41844361ffd629c93c514dba0f0c
= <matthewdegnan93@gmail.com>
2018-10-27 16:00:38 +0100

    Balance command

---
+++ commands/balance.js
@@ -0,0 +1,37 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const config = require(`../config.json`);
+
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./utils/users.db', sqlite3.OPEN_READWRITE, (err) => {
+  if(err){
+    console.error(err.message);
+  }
+  console.log(`Connected to DB - Balance`);
+});
+
+Number.prototype.format = function(n, x) {
+  var re = '(\\d)(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+  return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$1,');
+};
+
+exports.run = function(client, message, args){
+  let sql = `SELECT * FROM users WHERE id = ${message.author.id}`;
+  db.get(sql, [], (err, row) => {
+    if(err) return console.error(err.message);
+    if(!row) return message.reply(`You do not have a profile yet!`);
+    message.reply(`Your current balance is **\$${row.money.format(0)}`);
+  });
+};
+
+exports.conf = {
+  aliases: ['bal'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "test",
+  description: "Ping/Pong Command",
+  usage: "test"
+}
