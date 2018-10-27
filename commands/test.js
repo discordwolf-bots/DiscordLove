b849ca86791fcf66777964027c8ca8b164ced8f8
= <matthewdegnan93@gmail.com>
2018-10-27 16:13:10 +0100

    Table alterations fix #1

--- commands/test.js
+++ commands/test.js
@@ -12,9 +12,7 @@ let db = new sqlite3.Database('./utils/users.db', sqlite3.OPEN_READWRITE, (err) 
 });
 
 exports.run = function(client, message, args){
-  let sql = `ALTER TABLE users ADD achieve_your_value INTEGER DEFAULT 0,
-  achieve_owned_value INTEGER DEFAULT 0,
-  achhieve_buy_the_bot INTEGER DEFAULT 0`;
+  let sql = `ALTER TABLE users ADD achieve_your_value INTEGER DEFAULT 0, achieve_owned_value INTEGER DEFAULT 0, achieve_buy_the_bot INTEGER DEFAULT 0`;
   db.run(sql, [], (err) => {
     if(err) return console.error(err.message);
     console.log(`Table altered successfully`);
