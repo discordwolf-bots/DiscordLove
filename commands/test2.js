b1f52baaa2f1f345da1739a29e9a5c689c752a27
= <matthewdegnan93@gmail.com>
2018-10-27 16:37:17 +0100

    Table fix hope

--- commands/test2.js
+++ commands/test2.js
@@ -13,7 +13,9 @@ let db = new sqlite3.Database('./utils/users.db', sqlite3.OPEN_READWRITE, (err) 
 
 exports.run = function(client, message, args){
 
-  let sql = `ALTER TABLE users RENAME TO _users_old;`;
+  let sql = `INSERT INTO users (id, userID, money, cost, owner, lastpurchase, lastprofile, lastmessage)
+    SELECT id, userID, money, cost, owner, lastpurchase, lastprofile, lastmessage
+    FROM _users_old`;
   db.run(sql, (err) => {
     if(err) return console.error(err.message);
   });
@@ -26,7 +28,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: "test",
+  name: "test2",
   description: "Wolfs current test command",
-  usage: "test"
+  usage: "test2"
 }
