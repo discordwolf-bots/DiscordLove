9ecff98269fe6211d3024f57f376f6b5dafe5688
= <matthewdegnan93@gmail.com>
2018-10-31 23:58:10 +0000

    VC Switch payment

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -58,6 +58,8 @@ module.exports = (oldMember, newMember) => {
       db.run(sql2, (err) => {
         if(err) return console.error(err.message);
       });
+
+
     } else if(oldUserChannel === undefined && newUserChannel !== undefined){
       // User joins channel
       let sql2 = `UPDATE users SET voicejoined = ${now} WHERE id = ${row.id}`;
@@ -65,9 +67,10 @@ module.exports = (oldMember, newMember) => {
       db.run(sql2, (err) => {
         if(err) return console.error(err.message);
       });
+
+
     } else if(newUserChannel === undefined){
       // User leaves channel
-        client.channels.get(config.voice).send(`:x: ${oldMember.guild.member(row.id).user.username}#${oldMember.guild.members.get(row.id).user.discriminator} - **(${validTime} minutes)**`);
       if(row.voicejoined != 0){
 
         let timePassed = now - row.voicejoined;
@@ -87,6 +90,8 @@ module.exports = (oldMember, newMember) => {
           toAdd += random;
         }
 
+        client.channels.get(config.voice).send(`:x: ${oldMember.guild.member(row.id).user.username}#${oldMember.guild.members.get(row.id).user.discriminator} - **(${validTime} minutes)**`);
+
         let balance = row.money;
         let newBalance = balance + toAdd;
 
@@ -97,7 +102,44 @@ module.exports = (oldMember, newMember) => {
           if(err) return console.error(err.message);
         });
       }
+
+
+    } else if(oldUserChannel !== undefined && newUserChannel != undefined){
+      // User switches VC
+      if(row.voicejoined != 0){
+
+        let timePassed = now - row.voicejoined;
+        timePassed /= 1000;
+        let validTime = Math.floor(timePassed / 60); // How many minutes
+
+        console.log(validTime);
+
+        let min = 10;
+        let max = 20;
+        let toAdd = 0;
+
+        if(validTime == 0) return;
+
+        for(let i=1; i<=validTime; i++){
+          let random = Math.floor(Math.random() * (max-min)) + min;
+          toAdd += random;
+        }
+
+        client.channels.get(config.voice).send(`:performing_arts: ${oldMember.guild.member(row.id).user.username}#${oldMember.guild.members.get(row.id).user.discriminator} - **(${validTime} minutes)**`);
+
+        let balance = row.money;
+        let newBalance = balance + toAdd;
+
+        client.channels.get(config.logging).send(`:mega: VOICE CHAT SWITCH : ${oldMember.guild.member(row.id).user.username}#${oldMember.guild.members.get(row.id).user.discriminator} - ${row.money} -> ${newBalance} **(${validTime} minutes)**`);
+
+        let sql2 = `UPDATE users SET voicejoined = 0, money = ${newBalance}, voicetime = ${row.voicetime + validTime} WHERE id = ${row.id}`;
+        db.run(sql2, (err) => {
+          if(err) return console.error(err.message);
+        });
+      }
     }
+
+
   });
 
 
