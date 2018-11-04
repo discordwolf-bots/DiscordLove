d05b2e95908df31378113cf16987a2a03dcc5fa8
= <matthewdegnan93@gmail.com>
2018-11-04 21:16:10 +0000

    Logging for help

--- commands/fish.js
+++ commands/fish.js
@@ -114,10 +114,11 @@ exports.run = function(client, message, args){
          } else if(tDiffSecs == 1){
            tFormat += " " + tDiffSecs + " second";
          }
-         // if(message.author.id == config.botowner){
-         //   message.channel.send(`now = ${now}, row.lastfish = ${parseInt(row.lastfish)}`);
-         //   message.channel.send(`tDiff: ${tDiff}, tDiffMins = ${tDiffMins}, tDiffSecs = ${tDiffSecs}`);
-         // }
+
+         if(message.author.id == config.botowner){
+           message.channel.send(`tDiff = ${tDiff}, tDiffMins = ${tDiffMins}, tDiffSecs = ${tDiffSecs}`);
+         }
+
          return message.reply(`Please wait another **${tFormat}** before trying to fish again.`);
        }
 
