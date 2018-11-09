efd639364eee0f64f02fcbfd1402dfb1134359d0
= <matthewdegnan93@gmail.com>
2018-11-09 17:47:39 +0000

    Logging of all users

--- commands/test4.js
+++ commands/test4.js
@@ -13,30 +13,17 @@ let db = new sqlite3.Database('./utils/users.db', sqlite3.OPEN_READWRITE, (err) 
 
 exports.run = function(client, message, args){
 //482644639988580352
-  let sql = `SELECT * FROM users WHERE id = ${message.author.id}`;
-  db.get(sql, (err, row) => {
-    if(err) return console.error(err.message);
-    let sql2 = `UPDATE users SET money = ${row.money - 500} WHERE id = ${row.id}`;
-    db.run(sql2, (err) => {
-      if(err) return console.error(err.message)
-      message.channel.send(`Money reduced by \$$500 for user ${message.guild.members.get(row.id).user.username}#${message.guild.members.get(row.id).user.discriminator}`);
-    });
+  let sql = "SELECT * FROM users";
+  db.all(sql, (err, rows) => {
+    if(err) console.error(err.message);
+    rows.forEach((row) => {
+      console.log(`ID ${row.id} balance ${row.money}`);
+    })
   });
-
-  sql = `SELECT * FROM users WHERE id = '482644639988580352'`;
-  db.get(sql, (err, row) => {
-    if(err) return console.error(err.message);
-    sql2 = `UPDATE users SET money = ${row.money - 500} WHERE id = ${row.id}`;
-    db.run(sql2, (err) => {
-      if(err) return console.error(err.message)
-      message.channel.send(`Money reduced by \$$500 for user ${message.guild.members.get(row.id).user.username}#${message.guild.members.get(row.id).user.discriminator}`);
-    });
-  });
-
 };
 
 exports.conf = {
-  aliases: [],
+  aliases: ['check'],
   permLevel: 4
 };
 
