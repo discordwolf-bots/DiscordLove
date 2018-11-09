06708cb2f204351dfaad47ef9a4eaa20d9a058fb
= <matthewdegnan93@gmail.com>
2018-11-09 19:23:05 +0000

    Message Delete logging

---
+++ events/messageDelete.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const config = require(`../config.json`);
+const chalk = require('chalk');
+const moment = require('moment');
+
+const log = (msg) => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${msg}`);
+}
+
+module.exports = message => {
+  let guild = message.guild;
+  let client = message.client;
+  embed = new Discord.RichEmbed()
+    .setColor("#ff0000")
+    .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL)
+    .addField(`\u200b`, `${message.content}`)
+    .setFooter(`Message Deleted`)
+    .setTimestamp();
+  client.channels.get(config.delete).send({embed: embed});
+}
