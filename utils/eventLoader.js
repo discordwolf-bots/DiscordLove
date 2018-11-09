1a0a62d846ead87480a9bd75350ee093152603f1
= <matthewdegnan93@gmail.com>
2018-11-09 19:26:16 +0000

    Initiate the event

--- utils/eventLoader.js
+++ utils/eventLoader.js
@@ -2,8 +2,10 @@ const reqEvent = (event) => require(`../events/${event}`);
 module.exports = client => {
   client.on(`ready`, () => reqEvent(`ready`)(client));
   client.on(`message`, reqEvent(`message`));
+  client.on(`messageDelete`, reqEvent(`messageDelete`));
   client.on(`guildMemberAdd`, reqEvent('guildMemberAdd'));
   client.on(`guildMemberRemove`, reqEvent('guildMemberRemove'));
   client.on(`error`, reqEvent(`error`));
+  client.on(`messageDelete`, reqEvent(`messageDelete`));
   client.on('voiceStateUpdate', reqEvent(`voiceStateUpdate`));
 }
