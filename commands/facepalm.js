94c4259de6c7ab6b504069491bbe8ded3a773265
= <matthewdegnan93@gmail.com>
2018-11-11 18:12:10 +0000

    Facepalm Command

---
+++ commands/facepalm.js
@@ -0,0 +1,20 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const config = require(`../config.json`);
+
+exports.run = function(client, message, args){
+  message.delete();
+  message.channel.send(`(>ლ)`);
+};
+
+exports.conf = {
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "facepalm",
+  description: "Facepalm Command",
+  usage: "facepalm"
+}
