4e3145bc8485a082354f46bbfba205d830910535
= <matthewdegnan93@gmail.com>
2018-11-12 04:05:50 +0000

    Increase can now be used, achievement possibility added

--- commands/increase.js
+++ commands/increase.js
@@ -16,6 +16,41 @@ Number.prototype.format = function(n, x) {
   return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$1,');
 };
 
+const achievement_expensive_taste = (message, value, client) => {
+  let sql = `SELECT * FROM users WHERE id = ${message.author.id}`;
+  db.get(sql, (err, row) => {
+    if(err) return console.error(err.message);
+    let expensive_taste_progress = row.achieve_owned_value;
+    let achieved = 0;
+
+    for(let i=0; i<config.thresh_expensive_taste.length; i++){
+      if(value >= parseInt(config.thresh_expensive_taste.split(',')[i]))
+        if(expensive_taste_progress < i+1)
+          achieved = i+1;
+    }
+
+    if(achieved > 0){
+      let rewards = 0;
+      for(let i=expensive_taste_progress;i<=achieved;i++){
+        for(let j=1; j<=config.thresh_expensive_taste.length; j++){
+          if(i == j) rewards += parseInt(config.rewards.split(',')[j-1]);
+        }
+      }
+      let embed = new Discord.RichEmbed()
+        .setColor('#4DBF42')
+        .setAuthor(`Achievement Gained! - \$${rewards} added!`, message.author.avatarURL)
+        .setFooter(`Gained ${achieved-expensive_taste_progress} Levels on the Expensive Taste achievement`);
+      message.channel.send(embed);
+      let newBalance = row.money + rewards;
+      client.channels.get(config.logging).send(`:busts_in_silhouette: ACHIEVEMENT EXPENSIVE TASTE : ${message.author.username}#${message.author.discriminator} - ${row.money} -> ${newBalance}`);
+      let sqlUpdate = `UPDATE users SET money = ${newBalance}, achieve_owned_value = ${achieved} WHERE id = ${message.author.id}`;
+      db.run(sqlUpdate, (err) => {
+        if(err) return console.error(err.message);
+      });
+    }
+  });
+}
+
 exports.run = async function(client, message, args){
   let now = moment().format('x');
   let targetUser = message.mentions.members.first();
@@ -61,9 +96,10 @@ exports.run = async function(client, message, args){
         db.run(sql4, (err) => {
           if(err) return console.error(err.message);
 
-          message.reply(`You have successfully increased the price of <@${target}> to ${newCost.format(0)}`);
+          message.reply(`You have successfully increased the price of <@${target}> to **\$${newCost.format(0)}**`);
           client.channels.get(config.logging).send(`:dollar: INCREASE PRICE OWNER : ${message.author.username}#${message.author.discriminator} - ${ownerMoney} -> ${newMoney}`);
           client.channels.get(config.logging).send(`:heavy_dollar_sign: INCREASE PRICE TARGET : ${targetUser.user.username}#${targetUser.user.discriminator} - ${targetMoney} -> ${targetMoney+targetIncrease}`);
+          achievement_expensive_taste(message, newCost, client);
 
         });
       });
@@ -75,7 +111,7 @@ exports.run = async function(client, message, args){
 
 exports.conf = {
   aliases: [],
-  permLevel: 4
+  permLevel: 0
 };
 
 exports.help = {
