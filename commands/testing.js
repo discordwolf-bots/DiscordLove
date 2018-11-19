c308968efde980c06253661e4909f07bc32566b5
= <matthewdegnan93@gmail.com>
2018-11-19 17:07:02 +0000

    Tidied code

--- commands/testing.js
+++ commands/testing.js
@@ -6,14 +6,11 @@ const sqlite3 = require('sqlite3').verbose();
 
 exports.run = async function(client, message, args){
   client.guild_info(message.guild.id, '', (guild) => {
-    console.log(guild);
+    client.user_info(message.author.id, '', (user) => {
+      console.log(user);  
+      console.log(guild);
+    });
   });
-  client.user_info(message.author.id, '', (user) => {
-    console.log(user);
-  });
-
-  console.log(guild);
-  console.log(user);
 };
 
 exports.conf = {
