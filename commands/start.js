004cda2cc0806016a1156333c56a8c4358c27eb8
= <matthewdegnan93@gmail.com>
2018-11-19 17:29:31 +0000

    Start command tidied up

--- commands/start.js
+++ commands/start.js
@@ -13,28 +13,29 @@ let db = new sqlite3.Database('./utils/users.db', sqlite3.OPEN_READWRITE, (err) 
 
 exports.run = function(client, message, args){
   message.delete();
-  if(message.channel.name != 'discord-love-setup') return message.channel.send(`Please do the command **${config.prefix}start** in **discord-love-setup** first!`);
-  let discord_love_role = message.guild.roles.find(role => role.name === "DiscordLoved");
-  if(!discord_love_role) return message.channel.send(`Please create the role \`\`DiscordLoved\`\``);
-  let sql = `SELECT * FROM users WHERE user_discord = ${message.author.id}`;
-  db.get(sql, [], (err, row) => {
-    if(err) return console.error(err.message);
-    if(!row) {
-      let sqlInsert = `INSERT INTO users (user_discord) VALUES (${message.author.id})`;
-      db.run(sqlInsert, [], (err) => {
-        if(err) return console.error(err.message);
-        message.reply(`User profile created.`);
-        message.guild.members.get(message.author.id).addRole(discord_love_role.id);
-        console.log(`New user added : ${message.author.id}`);
-      });
-    } else {
-      if(message.member.roles.has(discord_love_role.id)){
-        message.reply(`You already have a profile! You dont need to set up again.`);
-      } else {
-        message.member.addRole(discord_love_role.id);
-        message.reply(`User profile found, role added.`);
+  client.guild_info(message.guild.id, '', (guild) => {
+    client.user_info(message.author.id, '', (user) => {
+
+      if(message.channel.name != 'discord-love-setup')
+        return message.channel.send(`Please do the command **${config.prefix}start** in **discord-love-setup** first!`);
+      let discord_love_role = message.guild.roles.find(role => role.name === "DiscordLoved");
+      if(!discord_love_role)
+        return message.channel.send(`Please create the role \`\`DiscordLoved\`\``);
+
+      if(!guild){
+        // Bot has not been added to the guild database, need a new function for this
+        return message.reply(`Please re-invite the bot to this server!`);
       }
-    }
+      if(!user){
+        // New user being added
+        let sqlInsert = `INSERT INTO users (user_discord) VALUES (${message.author.id})`;
+        db.run(sqlInsert, [], (err) => {
+          if(err) return console.error(err.message);
+        });
+      }
+      // Gives the member the DiscordLoved role
+      message.member.addRole(discord_love_role.id);
+    });
   });
 };
 
