e933690bf9a09320a4879dd790f9d72c2b164b20
= <matthewdegnan93@gmail.com>
2018-11-19 03:52:10 +0000

    attempt 7 - typo

--- utils/globalEvents.js
+++ utils/globalEvents.js
@@ -8,7 +8,7 @@ exports.db = new sqlite3.Database('./utils/users.db', sqlite3.OPEN_READWRITE, (e
   console.log(`Connected to DB - Index`);
 });
 
-exports.guild_info = function(guild, db) => {
+exports.guild_info = function(guild, db){
   let sql = `SELECT * FROM guilds WHERE guild_identifier = ${guild}`;
   db.get(sql, (err, row) => {
     if(err) return console.error(`message.js - ${err.message}`);
@@ -18,7 +18,7 @@ exports.guild_info = function(guild, db) => {
   });
 }
 
-exports.user_info = function(user, db) => {
+exports.user_info = function(user, db){
   let sql = `SELECT * FROM users WHERE user_discord = ${user}`;
   db.get(sql, (err, row) => {
     if(err) return console.error(`message.js - ${err.message}`);
