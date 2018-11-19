808b815b3c64c86fc3273375b66b448da0640b8a
= <matthewdegnan93@gmail.com>
2018-11-19 17:16:24 +0000

    Start fix and testing profile

--- commands/profile.js
+++ commands/profile.js
@@ -18,11 +18,11 @@ Number.prototype.format = function(n, x) {
 
 exports.run = function(client, message, args){
   message.delete();
-  let sql = `SELECT * FROM guilds WHERE guild_identifier = ${message.guild.id}`;
-  db.get(sql, [], (err, row) => {
-    if(err) return console.error(err.message);
-    if(!row) return message.reply(`You do not have a profile yet!`);
-    message.reply(row.guild_owner);
+  client.guild_info(message.guild.id, '', (guild) => {
+    client.user_info(message.author.id, '', (user) => {
+      if(!guild || !user) return;
+      message.reply(user.user_cps)
+    });
   });
 };
 
--- commands/start.js
+++ commands/start.js
@@ -14,6 +14,8 @@ let db = new sqlite3.Database('./utils/users.db', sqlite3.OPEN_READWRITE, (err) 
 exports.run = function(client, message, args){
   message.delete();
   if(message.channel.name != 'discord-love-setup') return message.channel.send(`Please do the command **${config.prefix}start** in **discord-love-setup** first!`);
+  let discord_love_role = message.guild.roles.find(role => role.name === "DiscordLoved");
+  if(!discord_love_role) return message.channel.send(`Please create the role \`\`DiscordLoved\`\``);
   let sql = `SELECT * FROM users WHERE user_discord = ${message.author.id}`;
   db.get(sql, [], (err, row) => {
     if(err) return console.error(err.message);
@@ -22,13 +24,16 @@ exports.run = function(client, message, args){
       db.run(sqlInsert, [], (err) => {
         if(err) return console.error(err.message);
         message.reply(`User profile created.`);
-        let discord_love_role = message.guild.roles.find(role => role.name === "DiscordLoved");
-        if(!discord_love_role) return message.channel.send(`Please create the role \`\`DiscordLoved\`\``);
         message.guild.members.get(message.author.id).addRole(discord_love_role.id);
         console.log(`New user added : ${message.author.id}`);
       });
     } else {
-      message.reply(`You already have a profile! You dont need to set up again.`);
+      if(message.member.roles.has(discord_love_role.id)){
+        message.reply(`You already have a profile! You dont need to set up again.`);
+      } else {
+        message.member.addRole(discord_love_role.id);
+        message.reply(`User profile found, role added.`);
+      }
     }
   });
 };
