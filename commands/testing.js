3033ab16da1bb0ae1d7a638a45af9c3ae16bcf93
= <matthewdegnan93@gmail.com>
2018-11-19 21:14:39 +0000

    typo

--- commands/testing.js
+++ commands/testing.js
@@ -9,7 +9,7 @@ exports.run = async function(client, message, args){
   client.guild_info(message.guild.id, '', (guild) => {
     client.user_info(message.author.id, '', (user) => {
       if(!guild || !user) return;
-      let sql = `UPDATE users SET user_cps = ${user_cps+0.5} WHERE user_discord = ${user.user_discord}`;
+      let sql = `UPDATE users SET user_cps = ${user.user_cps+0.5} WHERE user_discord = ${user.user_discord}`;
       client.db.run(sql, (err) => {
         if(err) return console.error(err.message);
       })
