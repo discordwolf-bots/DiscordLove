80b76e8fd44ca0ff3c5cafcb930a75c69a5436bd
= <matthewdegnan93@gmail.com>
2018-11-19 21:12:38 +0000

    CPS Test

--- commands/testing.js
+++ commands/testing.js
@@ -9,11 +9,9 @@ exports.run = async function(client, message, args){
   client.guild_info(message.guild.id, '', (guild) => {
     client.user_info(message.author.id, '', (user) => {
       if(!guild || !user) return;
-      let sql = `UPDATE users SET user_money = ${user.user_money + 0.5} WHERE user_discord = ${user.user_discord}`;
-      client.db.run(sql, (err)=> {
+      let sql = `UPDATE users SET user_cps = ${user_cps+0.5} WHERE user_discord = ${user.user_discord}`;
+      client.db.run(sql, (err) => {
         if(err) return console.error(err.message);
-        console.log(user);
-        console.log(typeof user.user_money);
       })
     });
   });
