270d020f21718aa36f9695779f758162cb2865ea
= <matthewdegnan93@gmail.com>
2018-11-28 21:33:15 +0000

    typo

--- commands/profile.js
+++ commands/profile.js
@@ -32,7 +32,7 @@ exports.run = function(client, message, args){
       // Format username
       let display_name = message.member.displayName;
       if(user.premium_status > 0) display_name = `**[PREMIUM]** ${display_name}`;
-      profile.push(`Profile of ${displayName}`);
+      profile.push(`Profile of ${display_name}`);
 
       // Get users money
       profile.push(`Bank: **$${user.user_money.format(2)}**`);
